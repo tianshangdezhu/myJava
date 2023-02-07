5d0b8f24981b43c92f18e1ecbb662f8d93b479be
tianshangdezhu <sffdeyouxiang@126.com>
2023-02-07 21:55:11 +0800

    项目初始化

---
+++ mybatis_plus/src/main/java/com/sff/MybatisPlusApplication.java
@@ -0,0 +1,12 @@
+package com.sff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApplication.class, args);
+    }
+}
